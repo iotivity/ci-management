8e3d7c6fb809c4f4923e7d8f326debf99ec39ac7
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-01 11:49:26 -0700

    Move boost location in Windows Bootstrap
    Change-Id: If71107b2b4a6275a88eaa2720afe0d88bc9209a1
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
+++ jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
@@ -1,4 +1,6 @@
 @echo off
 echo "Copying dependencies..."
-xcopy /q /e C:\j\e\boost\* %WORKSPACE%\extlibs\boost\
+mklink %WORKSPACE%\extlibs\boost\boost_1_60_0.zip C:\j\e\boost_1_60_0.zip
+md %WORKSPACE%\extlibs\boost\boost\
+xcopy /q /e C:\j\e\boost\* %WORKSPACE%\extlibs\boost\boost
 xcopy /q /e C:\j\e\tinycbor %WORKSPACE%\extlibs\tinycbor\
