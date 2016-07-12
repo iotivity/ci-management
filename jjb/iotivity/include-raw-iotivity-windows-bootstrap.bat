63952ff8e8228e8ee8dbdacc2714cbbc8715af75
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-12 08:28:43 -0700

    Link gtest zip before building Windows
    Change-Id: Icf53f81108182b4ac89455463b1d6a695c3af7c7
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
+++ jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
@@ -1,5 +1,6 @@
 @echo off
 echo "Copying dependencies..."
+mklink %WORKSPACE%\extlibs\gtest\gtest-1.7.0.zip C:\j\e\gtest-1.7.0.zip
 mklink %WORKSPACE%\extlibs\boost\boost_1_60_0.zip C:\j\e\boost_1_60_0.zip
 md %WORKSPACE%\extlibs\boost\boost\
 xcopy /q /e C:\j\e\boost\* %WORKSPACE%\extlibs\boost\boost
