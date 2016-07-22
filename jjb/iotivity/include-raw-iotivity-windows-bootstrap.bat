c1ac152ee0cf5fe47f39417a39f8f5eca3a5c488
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-22 15:26:18 -0700

    Use version 0.2.1 of tinycbor on all platforms
    Change-Id: If1829132ce34c3a352aee568317866f6967080e9
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
+++ jjb/iotivity/include-raw-iotivity-windows-bootstrap.bat
@@ -4,4 +4,4 @@ mklink %WORKSPACE%\extlibs\gtest\gtest-1.7.0.zip C:\j\e\gtest-1.7.0.zip
 mklink %WORKSPACE%\extlibs\boost\boost_1_60_0.zip C:\j\e\boost_1_60_0.zip
 md %WORKSPACE%\extlibs\boost\boost\
 xcopy /q /e C:\j\e\boost\* %WORKSPACE%\extlibs\boost\boost
-xcopy /q /e C:\j\e\tinycbor %WORKSPACE%\extlibs\tinycbor\
+xcopy /q /e C:\j\e\tinycbor\tinycbor-0.2.1 %WORKSPACE%\extlibs\tinycbor\
