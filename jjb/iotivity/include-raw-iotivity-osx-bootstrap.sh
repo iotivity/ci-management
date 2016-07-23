ffb4e09ac6db5a1fa01eb8547d36d32a48b5cbde
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-22 19:32:16 -0700

    Remove lingering double-quote from OSX bootstrap
    Change-Id: I5217d76c4fc20d022b02add5a0b06964810ae85a
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
+++ jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
@@ -7,4 +7,4 @@ TINYCBOR_VERSION='0.2.1'
 unzip -oq "${IOTIVITYEXTLIB}/tinycbor/v${TINYCBOR_VERSION}.zip" -d ${IOTIVITYEXTLIB}/tinycbor/
 ln -s "${IOTIVITYEXTLIB}/tinycbor/tinycbor-${TINYCBOR_VERSION}" ${WORKSPACE}/extlibs/tinycbor/tinycbor
 unzip -oq ${IOTIVITYEXTLIB}/gtest/gtest-1.7.0.zip -d ${WORKSPACE}/extlibs/gtest
-ln -s "${IOTIVITYEXTLIB}/gtest/gtest-1.7.0.zip ${WORKSPACE}/extlibs/gtest/gtest-1.7.0.zip
+ln -s ${IOTIVITYEXTLIB}/gtest/gtest-1.7.0.zip ${WORKSPACE}/extlibs/gtest/gtest-1.7.0.zip
