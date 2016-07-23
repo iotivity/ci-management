f67dcb23f7e8f67fa79c815705b5c80122982c45
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-07-22 18:41:46 -0700

    Update OSX Bootstrap to extract gtest
    Change-Id: Ia7dc9c9043564fabe109c26867b9cfc641c327cd
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
+++ jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
@@ -5,4 +5,6 @@ IOTIVITYEXTLIB=/Users/jenkins-ci/extlibs
 TINYCBOR_VERSION='0.2.1'
 
 unzip -oq "${IOTIVITYEXTLIB}/tinycbor/v${TINYCBOR_VERSION}.zip" -d ${IOTIVITYEXTLIB}/tinycbor/
-ln -s "/Users/jenkins-ci/extlibs/tinycbor/tinycbor-${TINYCBOR_VERSION}" $WORKSPACE/extlibs/tinycbor/tinycbor
+ln -s "${IOTIVITYEXTLIB}/tinycbor/tinycbor-${TINYCBOR_VERSION}" ${WORKSPACE}/extlibs/tinycbor/tinycbor
+unzip -oq ${IOTIVITYEXTLIB}/gtest/gtest-1.7.0.zip -d ${WORKSPACE}/extlibs/gtest
+ln -s "${IOTIVITYEXTLIB}/gtest/gtest-1.7.0.zip ${WORKSPACE}/extlibs/gtest/gtest-1.7.0.zip
