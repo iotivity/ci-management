aff88d014d4d2cf8ffc276f8d33e3cb08cd8a327
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-08-31 14:08:44 -0700

    Ensure 1.1-rel is verified against tinycbor v0.2.1 on OSX
    Change-Id: I473308d75a02348abb2a25b7eacc03c37cb1d4e1
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
+++ jjb/iotivity/include-raw-iotivity-osx-bootstrap.sh
@@ -3,6 +3,9 @@ set -o pipefail
 PATH=$PATH:/usr/local/bin/
 IOTIVITYEXTLIB=/Users/jenkins-ci/extlibs
 TINYCBOR_VERSION='0.3.2'
+if [ "$GERRIT_BRANCH" = "1.1-rel" ]; then
+    TINYCBOR_VERSION='0.2.1'
+fi
 
 unzip -oq "${IOTIVITYEXTLIB}/tinycbor/v${TINYCBOR_VERSION}.zip" -d ${IOTIVITYEXTLIB}/tinycbor/
 ln -s "${IOTIVITYEXTLIB}/tinycbor/tinycbor-${TINYCBOR_VERSION}" ${WORKSPACE}/extlibs/tinycbor/tinycbor
