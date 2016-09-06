c70d5a5525beeec94ebd74100251b46373300757
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-09-06 08:58:35 -0700

    Unmount Tizen build root before each build
    Change-Id: Iab3fb965f5523a10989b6b3c7ad2d8b3e06d383b
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

---
+++ jjb/iotivity/include-raw-iotivity-tizen.sh
@@ -0,0 +1,11 @@
+BUILD_ROOTS="GBS-ROOT GBS-ROOT-RI GBS-ROOT-ES GBS-ROOT-OIC GBS-ROOT-RI-OIC"
+
+for build_dir in $BUILD_ROOTS; do
+  if [ -d "$HOME/${build_dir}" ]; then
+    echo "$build_dir Exists. Ensuring clean chroot"
+    chroot_dir="$HOME/${build_dir}/local/BUILD-ROOTS/scratch.armv7l.0"
+    [ -d $chroot_dir/proc ] && sudo /bin/umount -l -f $chroot_dir/proc || true
+    [ -d $chroot_dir/dev/pts ] && sudo /bin/umount -l -f $chroot_dir/dev/pts || true
+    [ -d $chroot_dir/dev/shm ] && sudo /bin/umount -l -f $chroot_dir/dev/shm || true
+  fi
+done
