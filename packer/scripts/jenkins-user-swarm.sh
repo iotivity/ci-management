58e83b043df99cfe68babdc035e3c34c7163517d
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-11-11 14:44:09 -0800

    Jenkins Swarm Service and Script Updates
    - Pulls Jenkins hostname from metadata
    - Uses environment variable for labels
    
    Change-Id: Ia96bd3e597c5cab928ca7016641ba6b0f3acbe53
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- packer/scripts/jenkins-user-swarm.sh
+++ packer/scripts/jenkins-user-swarm.sh
@@ -4,27 +4,49 @@ set -o pipefail
 # Create User
 useradd -U -k /etc/skel -c "Jenkins User" -m jenkins-ci -d /home/jenkins-ci -s /bin/bash
 
+# Create Swarm Init Script
+cat << EOF >> /etc/init/jenkins-ci
+# jenkins-ci - Jenkins Swarm Client
+#
+# This service starts the Jenkins Swarm agent once the metadata server
+# is available
+
+description "Jenkins Swarm Client"
+
+start on filesystem or runlevel [2345]
+stop on runlevel [!2345]
+
+respawn
+respawn limit 10 5
+
+env user=jenkins-ci
+env cmd=/home/jenkins-ci/jenkins-swarm.sh
+env HOME=/home/jenkins-ci
+
+exec su -s /bin/sh -c 'exec "\$0" "\$@"' \$user -- \$cmd
+EOF
+
 # Download Swarm
 wget -nv 'https://repo.jenkins-ci.org/releases/org/jenkins-ci/plugins/swarm-client/2.2/swarm-client-2.2-jar-with-dependencies.jar' -O /home/jenkins-ci/swarm.jar
 
 # Create Swarm Connection Script
 cat << EOF >> /home/jenkins-ci/jenkins-swarm.sh
-#! /bin/bash -ex
+#! /bin/bash -e
 
 set -o pipefail
 
 # Collect Metadata resources
-curl -L -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_labels > labels.txt
-
-export JENKINS_USERNAME="\$(curl -L -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_username)"
-export JENKINS_PASSWORD="\$(curl -L -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_api_token)"
+export JENKINS_LABELS="\$(curl -sL -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_labels)"
+export JENKINS_HOSTNAME="\$(curl -sL -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_hostname)"
+export JENKINS_USERNAME="\$(curl -sL -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_username)"
+export JENKINS_PASSWORD="\$(curl -sL -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/attributes/jenkins_api_token)"
 
 # Connect to Jenkins
-java -jar swarm-client-2.2-jar-with-dependencies.jar \
- -master http://build.iotivity.org:8080/ci/ \
+java -jar \$HOME/swarm.jar \
+ -master http://\$JENKINS_HOSTNAME:8080/ci/ \
  -executors 1 \
  -description "IoTivity Swarm Client" \
- -labelsFile labels.txt \
+ -labels \$JENKINS_LABELS \
  -passwordEnvVariable JENKINS_PASSWORD \
  -mode exclusive \
  -name \$HOSTNAME \
