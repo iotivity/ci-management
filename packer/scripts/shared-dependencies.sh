3a8b8d9b68e6bebf0733d831ec96d9c9a9b407f6
Trevor Bramwell <tbramwell@linuxfoundation.org>
2016-11-15 12:35:39 -0800

    Ensure git tag for mbedtls-2.4.0 exists
    Change-Id: I9a9f6ca8d12509fb9d181b08128b92e8d35111ab
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- packer/scripts/shared-dependencies.sh
+++ packer/scripts/shared-dependencies.sh
@@ -73,4 +73,5 @@ git clone https://github.com/dascandy/hippomocks /extlibs/hippomocks-master \
 mkdir -p /extlibs/mbedtls
 git clone https://github.com/ARMmbed/mbedtls /extlibs/mbedtls/mbedtls \
 && cd /extlibs/mbedtls/mbedtls \
-&& git fetch --all --tags
+&& git fetch --all --tags \
+&& echo "bbdbbda04f88545fa525490a64aac84e6cf08a6c" > .git/refs/tags/mbedtls-2.4.0
