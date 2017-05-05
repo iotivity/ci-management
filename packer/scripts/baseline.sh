cbddc5d6681155fef974298bedf9b1a40b0530cd
Trevor Bramwell <tbramwell@linuxfoundation.org>
2017-05-05 11:26:59 -0700

    Make Java 8 Default on Linux Builders
    This is required for using the new Swarm plugin
    
    Change-Id: I658b94d576618d4b5c788fc3a7456c04681ab4e3
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- packer/scripts/baseline.sh
+++ packer/scripts/baseline.sh
@@ -17,7 +17,7 @@ apt-get -y -q install default-jre openjdk-7-jdk openjdk-8-jdk
 
 apt-get -y -q upgrade
 
-update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
-update-alternatives --set javac /usr/lib/jvm/java-7-openjdk-amd64/bin/javac
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+update-alternatives --set javac /usr/lib/jvm/java-8-openjdk-amd64/bin/javac
 
 update-ca-certificates -f
