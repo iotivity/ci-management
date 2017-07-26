951c31d6943b237a0b70d2a26d14c237ff1c98a8
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Windows Packer support for VEXXHOST
    Use AdminDeployment.xml from production VS2015 Windows Server
    2012-R2 system to install Visual Studio
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/win for windows-specific provisioning files
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-vs2015.ps1
@@ -0,0 +1,77 @@
+$vs_iso="en_visual_studio_professional_2015_with_update_3_x86_x64_dvd_8923272.iso"
+$vs_iso_url="http://moonunit.colliertech.org/images/$vs_iso"
+
+#$vs_installer="en_visual_studio_professional_2015_with_update_3_x86_x64_web_installer_8922978.exe"
+$vs_installer="vs_professional.exe"
+$vs_hash="95-9E-20-67-2E-1A-8B-BC-8E-46-BA-FD-22-66-B4-FD"
+echo "vs_installer=$vs_installer"
+
+$vs_adminfile="C:\Packer\AdminDeployment-ws2012-vs2015.xml"
+echo "vs_adminfile=$vs_adminfile"
+
+$vs_url="https://my.visualstudio.com/Downloads?pid=2088"
+echo "vs_url=$vs_url"
+$vs_url="http://moonunit.colliertech.org/~cjac/tmp/$vs_installer"
+
+$curl_bin="C:\util\curl.exe"
+
+#$abs_vs_installer="$HOME\Downloads\$vs_installer"
+$installer_drive="D:"
+$abs_vs_installer="$installer_drive\$vs_installer"
+$abs_vs_iso="$HOME\Downloads\$vs_iso"
+
+if(![System.IO.File]::Exists($abs_vs_installer)){
+#  echo "c:\util\curl.exe -o $abs_vs_installer $vs_url"
+  echo "c:\util\curl.exe -o $abs_vs_iso $vs_iso_url"
+
+#  & $curl_bin -o $abs_vs_installer $vs_url
+  & $curl_bin -o $abs_vs_iso $vs_iso_url
+
+#  if(![System.IO.File]::Exists($abs_vs_installer)){
+  if(![System.IO.File]::Exists($abs_vs_iso)){
+    echo "Visual Studio ISO was not downloaded"
+    exit 1
+  }
+}
+
+Mount-DiskImage $abs_vs_iso
+
+#$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+#$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($abs_vs_installer)))
+
+#if ( $hash -eq $vs_hash ){
+#  echo "hash match.  successful download"
+#}else{
+#  echo "hash mismatch"
+#  echo "$vs_hash expected"
+#  echo "$hash received"
+#  exit 2
+#}
+
+Get-Date -f u
+
+echo "$abs_vs_installer /adminfile $vs_adminfile /quiet /norestart"
+& $abs_vs_installer /adminfile $vs_adminfile /quiet /norestart
+
+$slept = 0
+do {
+  sleep 5
+  $slept += 5
+  Write-Host -NoNewline "."
+} while ( [bool](Get-Process vs_professional*) )
+
+Write-Host "Done"
+Get-Date -f u
+
+Write-Host "Install took $slept seconds!"
+
+if ( $slept -lt 100 ){
+  echo "Installation period was too short.  Failing."
+  Select-String -Path $HOME\AppData\Local\Temp\dd_vs_professional_[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9].log -Pattern error
+  exit 3
+}
+
+
+# TODO: check $PATH for expected utilities
+
+
