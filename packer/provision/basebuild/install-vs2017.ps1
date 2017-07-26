b176e0e5ad584612f1bfa8433c0e24887a80b070
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* corrected partition resize code + tested
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.md
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* as OpenStack user-data scripts
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-vs2017.ps1
@@ -0,0 +1,61 @@
+echo "Installing Visual Studio 2017"
+
+choco install -y windows-sdk-10.1
+
+#if( $? -ne 0 ){
+#&  type C:\Users\Admin\AppData\Local\Temp\chocolatey\windows-sdk-10.1*.log
+#}
+
+choco install -y jdk8
+
+$win_sdk_ver="15063"
+
+echo "Adding Visual Studio and tools to PATH"
+if (!(Test-Path $profile)) {
+  mkdir -force ([system.io.fileinfo]$profile).DirectoryName
+
+  echo '$env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"' > $profile
+}
+
+echo "Disk usage:"
+# Get-WmiObject win32_logicaldisk
+Get-WmiObject Win32_LogicalDisk -filter "DeviceID = 'c:' "
+
+$c_disk=Get-Volume 'c'
+
+#$vsSource="https://aka.ms/vs/15/release/vs_community.exe"
+$vsSource="https://download.visualstudio.microsoft.com/download/pr/100359697/045b56eb413191d03850ecc425172a7d/vs_Community.exe"
+$vsDest="c:\packer\vs_community.exe"
+& c:\ProgramData\chocolatey\bin\curl.exe -k -o $vsDest $vsSource
+& $vsDest `
+    --add Microsoft.VisualStudio.Workload.Universal `
+    --add Microsoft.VisualStudio.Component.VC.140 `
+    --add Microsoft.VisualStudio.Component.VC.CoreIde `
+    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
+    --add Microsoft.VisualStudio.Component.Windows81SDK `
+    --add Microsoft.VisualStudio.Component.Windows10SDK `
+    --add Microsoft.VisualStudio.Component.Windows10SDK.$win_sdk_ver.Desktop `
+    --add Microsoft.VisualStudio.Component.Windows10SDK.$win_sdk_ver.UWP `
+    --add Microsoft.VisualStudio.ComponentGroup.UWP.VC `
+    --add Microsoft.VisualStudio.Component.UWP.Support `
+    --add Microsoft.Component.VC.Runtime.OSSupport `
+    --add Microsoft.Component.NetFX.Native `
+    --add Microsoft.Component.MSBuild `
+    --add Microsoft.Net.Component.4.5.TargetingPack `
+    --add Microsoft.Net.Component.4.7.SDK `
+    --add Microsoft.Net.Component.4.7.TargetingPack `
+    --add Component.Android.SDK23 `
+    --quiet --wait --norestart
+
+echo "visual studio installer executed.  Waiting for 60m for completion..."
+
+# sleep for 60 minutes
+Start-Sleep -s 3600
+
+# check $PATH for expected utilities
+if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\cl.exe") {
+  echo "Visual Studio 2017 Installed"
+}
+
+# TODO: check include path for expected headers
+
