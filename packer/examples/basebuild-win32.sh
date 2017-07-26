cea3c9c243eb8ac1e2cf1216f14579f901f2fa3f
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* increase timeout killer duration from 10m to 35m
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/examples/basebuild-win32.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# https://www.packer.io/docs/builders/qemu.html
+
+#perl gen-packer.pl > packer.json
+
+mkdir -p temp log
+
+TMPDIR=./temp
+export PACKER_LOG=1
+TS=$(date +"%Y%m%dT%H%M%S")
+export PACKER_LOG_PATH="${PWD}/log/packer-win-basebuild-i386-${TS}.log"
+
+${HOME}/bin/packer build   \
+  -var-file=vars/cloud-env.json   \
+  -var-file=vars/windows-server-2012-i386-basebuild.json   \
+  templates/basebuild-win.json
+
+unset -v TS
