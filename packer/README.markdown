34710e9db8e6fd4cf68d56e51b9393e2563adc0d
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Windows Packer support for VEXXHOST
    /* refactoring order of variabl definitions and function calls */
    
    Use AdminDeployment.xml from production VS2015 Windows Server
    2012-R2 system to install Visual Studio
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/win for windows-specific provisioning files
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/README.markdown
@@ -0,0 +1,60 @@
+# IoTivity CI Packer
+
+[Packer][1] is a tool for automatically creating VM and container images,
+configuring them and post-processing them into standard output formats.
+
+We build IoTivity's CI images via Packer.
+
+## Building
+
+You'll need to [install Packer][2], of course.
+
+IoTivity's Packer configuration is divided into build-specific variables,
+output-specific templates and a set of shared provisioning scripts. To do a
+specific build, combine the template for the desired output artifact type with
+a variable file. To build a new java-builder instance the following would be done:
+
+```
+packer build \
+  -var-file=vars/cloud-env.json \
+  -var-file=vars/centos-7.json \
+  templates/java-builder.json
+```
+or
+```
+packer build \
+  -var-file=vars/cloud-env.json \
+  -var-file=vars/centos-7.json \
+  templates/baseline.json
+```
+or
+```
+packer build \
+  -var-file=vars/cloud-env.json \
+  -var-file=vars/windows-server-2012.json \
+  templates/baseline-win.json
+```
+
+
+**NOTE:** vars/cloud-env.json is a gitignored file as it contains private
+information. There is a vars/cloud-env.json.example file that may be used as a
+base for creating the one needed.
+
+This would build bootable image in two different OpenStack clouds. In specific,
+Rackspace's Public cloud and a private OpenStack cloud.
+
+From a high level, the builds:
+
+* Boot a specified base image in both clouds.
+* Run a set of shell scripts, listed in the template's shell provisioner
+  section, to do any configuration required by the builder.
+* Execute a shutdown of the running instance in the clouds.
+* Execute a 'nova image-create' operation against the shutdown instance.
+* Perform a 'nova delete' operation against the shutdown instance.
+
+Windows scripts were sourced from Jakob Aarøe Dam's fine
+packer-qemu-templates[3] repository.
+
+[1]: https://www.packer.io/
+[2]: https://www.packer.io/intro/getting-started/setup.html
+[3]: https://github.com/jakobadam/packer-qemu-templates/tree/master/windows
---
+++ packer/provision/baseline.ps1
@@ -0,0 +1,269 @@
+#!/usr/bin/env powershell
+# vi: ts=4 sw=4 sts=4 et :
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+
+$download_dir="$HOME\Downloads\"
+$tmp_dir="$HOME\AppData\Local\Temp\"
+$util_dir="C:\Util\"
+$deps_dir="C:\j\e\"
+$dst_dir=$util_dir
+$curl_path=$util_dir+"curl.exe"
+
+if (!(Test-Path $dst_dir)) {
+  mkdir -Force $dst_dir
+  echo "destination directory [$dst_dir] created"
+}
+
+function check-hash
+{
+  $filename=$args[0]
+  $expected_hash=$args[1]
+  $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($filename)))
+
+  $is_match = $hash -eq $expected_hash
+
+  if( !$is_match ){
+    echo "$expected_hash expected"
+    echo "$hash received"
+  }
+
+  return( $is_match )
+}
+
+function curl
+{
+  $src=$arg[0]
+  $dst=$arg[1]
+
+  if(![System.IO.File]::Exists($dst)){
+    if([System.IO.File]::Exists($curl_path)){
+      & $curl_path -o $dst $src
+    }else{
+      wget $src -OutFile $dst
+    }
+  }
+
+  if(![System.IO.File]::Exists($dst)){
+    echo "file $src was not downloaded to $dst"
+    exit 1
+  }
+}
+
+function fetch-git-repo
+{
+  $repo_name=$args[0]
+  $github_path=$args[1]
+  $tag=$args[2]
+
+  $github_url="https://github.com/$github_path.git"
+  $checkout_dir="$deps_dir$repo_name\$repo_name"
+
+  & git clone $github_url $checkout_dir
+  cd $checkout_dir
+  & git checkout $tag
+}
+
+function fetch-zip
+{
+  $src=$args[0]
+  $zipfile=$args[1]
+  $expected_hash=$args[2]
+
+  $abs_zipfile=$download_dir+$zipfile
+
+  curl( $src, $abs_zipfile )
+
+  if ( -Not check-hash( $abs_zipfile, $expected_hash ) ){
+    Remove-Item $abs_zipfile
+    exit 2
+  }
+
+  echo "zipfile downloaded to $abs_zipfile"
+}
+
+function fetch-zipexe
+{
+  $version=$args[0]
+  $zipfile=$args[1]
+  $bin=$args[2]
+  $src_fmt=$args[3]
+  $expected_hash=$args[4]
+
+  if( $src_fmt -Match "{1}" ){
+    $src = $src_fmt -f $version, $zipfile
+  }elseif($src_fmt -Match "{0}" -and
+          $zipfile -Match "{0}" ){
+    $zipfile = $zipfile -f $version
+    $src = $src_fmt -f $zipfile
+  }else{
+    echo "source unrecognized: $src_fmt"
+    exit 1
+  }
+
+  fetch-zip($src, $zipfile, $expected_hash)
+
+  $abs_zipfile=$download_dir+$zipfile
+  $tmp_dir="$HOME\AppData\Local\Temp\$zipfile"
+
+  if (!(Test-Path $tmp_dir)) {
+    mkdir -Force $tmp_dir
+    echo "temp directory [$tmp_dir] created"
+  }
+
+  [System.IO.Compression.ZipFile]::ExtractToDirectory($abs_zipfile, $tmp_dir)
+
+  if(![System.IO.File]::Exists($tmp_dir+$bin)){
+    echo "[$bin] does not exist within zipfile [$zipfile]"
+    Remove-Item $abs_zipfile
+    exit 3
+  }
+
+  Copy-Item $tmp_dir+$bin $dst_dir+$bin
+  if(![System.IO.File]::Exists($dst_dir+$bin)){
+    echo "could not copy [$bin] to [$dst_dir]"
+  }
+}
+
+function fetch-exe
+{
+  $version=$args[0]
+  $bin=$args[1]
+  $src_fmt=$args[2]
+
+  $dst=$dst_dir + $bin
+  $src=$src_fmt -f $version, $pkg
+
+  $dst=$util_dir+$bin
+
+  curl( $src, $tmp_dir+$bin )
+
+  if ( -Not check-hash( $tmp_dir+$bin, $expected_hash ) ){
+    Remove-Item $tmp_dir+$bin
+    exit 2
+  }
+
+  Copy-Item $tmp_dir+$bin $dst_dir+$bin
+
+}
+
+
+
+echo "fetching cURL"
+fetch-zipexe( "754_1_ssl",
+              "curl_{0}.zip",
+              "curl.exe",
+              "http://www.paehl.com/open_source/?download={0}" )
+
+#echo "fetching PSWindowsUpdate"
+#& C:\packer\install-PSWindowsUpdate.ps1
+#echo "updating windows"
+#$cred = get-credential
+#Invoke-Command -ComputerName localhost -credential $cred -scriptblock {& C:\packer\win-updates.ps1}
+#& C:\packer\windows-update.ps1
+
+echo "removing password expiry"
+& C:\packer\unlimited-password-expiration.bat
+echo "enabling RDP"
+& C:\packer\enable-rdp.bat
+echo "disabling UAC"
+& C:\packer\uac-disable.bat
+echo "disabling hibernate"
+& C:\packer\disable-hibernate.bat
+
+#
+# Nuget
+#
+fetch-exe( "latest",
+	   "nuget.exe",
+	   "https://dist.nuget.org/win-x86-commandline/{0}/{1}}" )
+
+#
+# ZIP FILES
+#
+# boost_1_60_0.zip
+# https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.zip
+#
+# gtest-1.7.0.zip
+# https://codeload.github.com/google/googletest/zip/release-1.7.0
+#
+# sqlite3
+# https://www.sqlite.org/2017/sqlite-amalgamation-3200000.zip
+#
+# CMake
+# https://cmake.org/files/v3.9/cmake-3.9.1-win64-x64.zip
+# https://cmake.org/files/v3.9/cmake-3.9.1-win32-x86.zip
+
+fetch-git-repo( "libcoap", "dthaler/libcoap", "IoTivity-1.2.1c")
+fetch-git-repo( "mbedtls", "armmbed/mbedtls", "mbedtls-2.4.2")
+fetch-git-repo( "tinycbor", "01org/tinycbor", "v0.4.1")
+
+echo "connect to system console and perform windows update now."
+echo "When finished, create a file $HOME\AppData\Local\Temp\update-done.txt"
+
+$slept = 0
+Get-Date -f u
+do {
+  sleep 5
+  $slept += 5
+  Write-Host -NoNewline "."
+} while(![System.IO.File]::Exists("$HOME\AppData\Local\Temp\update-done.txt"))
+rm $HOME\AppData\Local\Temp\update-done.txt
+echo "Done"
+Get-Date -f u
+
+echo "fetching vs2015"
+& C:\packer\fetch-vs2015.ps1
+
+echo "Installing vs2015"
+& C:\packer\install-vs2015.ps1
+
+#
+# INSTALLERS
+#
+# Python
+#
+# version=2.7.13
+# https://www.python.org/ftp/python/$version/python-$version.msi
+
+$file=python-2.7.13.msi
+
+curl( "http://moonunit.colliertech.org/~cjac/tmp/$file",
+      $download_dir+$file )
+#
+# pywin32
+#
+# https://newcontinuum.dl.sourceforge.net/project/pywin32/pywin32/Build%20221/pywin32-221.win32-py2.7.exe
+
+$file=pywin32-221.win32-py2.7.exe
+
+curl( "http://moonunit.colliertech.org/~cjac/tmp/$file",
+      $download_dir+$file )
+
+#
+# git
+#
+# https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/Git-2.14.1-64-bit.exe
+
+$file=Git-2.14.1-64-bit.exe
+
+curl( "http://moonunit.colliertech.org/~cjac/tmp/$file",
+      $download_dir+$file )
+
+echo "connect to system console and install from packages in $download_dir now."
+echo "When finished, create a file $HOME\AppData\Local\Temp\update-done.txt"
+
+$slept = 0
+Get-Date -f u
+do {
+  sleep 5
+  $slept += 5
+  Write-Host -NoNewline "."
+} while(![System.IO.File]::Exists("$HOME\AppData\Local\Temp\update-done.txt"))
+rm $HOME\AppData\Local\Temp\update-done.txt
+echo "Done"
+Get-Date -f u
+
+echo "disabling windows update"
+& C:\packer\disablewinupdate.bat
