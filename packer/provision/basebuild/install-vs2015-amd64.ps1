a77805f92bdd344d224a67cb0bcfccf24f63a95c
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Windows Packer support for VEXXHOST
    /* pausing for 2m after running sysprep
     * unpack boost after fetching it
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-vs2015-amd64.ps1
@@ -0,0 +1,13 @@
+echo "Installing Visual Studio 2015"
+
+#choco install -y VisualStudio2015Community
+#choco install -y VisualStudio2015Professional
+#choco install -y visualstudio2015-update
+
+#choco install -y vcredist2015
+
+choco install --execution-timeout 0 -y vcbuildtools -ia "/Full"
+
+# TODO: check $PATH for expected utilities
+
+echo "Visual Studio 2015 Installed"
