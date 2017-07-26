8a084431c8ab13db625326739c666917b41ca965
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* use manual install rather than chocolatey
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-vs2015-amd64.ps1
@@ -0,0 +1,37 @@
+echo "Installing Visual Studio 2015"
+
+echo "Disk usage:"
+Get-WmiObject win32_logicaldisk
+
+
+#choco install -y VisualStudio2015Professional
+#choco install -y visualstudio2015-update
+
+#choco install --execution-timeout 0 -y KB3033929 chocolatey-windowsupdate.extension KB3035131 KB2999226 choco install vcredist2015
+#choco install --execution-timeout 0 -y vcredist-all
+
+#choco install --execution-timeout 0 -y vcbuildtools -ia "/Full"
+#choco install --execution-timeout 0 -y mingw
+#choco install --execution-timeout 0 -y vim
+#choco install --execution-timeout 0 -y windows-sdk-7.1
+#choco install --execution-timeout 0 -y windows-sdk-8.1
+choco install --execution-timeout 0 -y windows-sdk-10.1
+
+$vsSource="https://download.microsoft.com/download/0/B/C/0BC321A4-013F-479C-84E6-4A2F90B11269/vs_community.exe"
+$vsDest="c:\packer\vs_community.exe"
+& c:\ProgramData\chocolatey\bin\curl.exe -k -o $vsDest $vsSource
+& $vsDest /Quiet /NoRestart /AdminFile c:\packer\AdminDeployment-ws2012-vs2015.xml
+# sleep for 25 minutes
+Start-Sleep -s 1500
+
+if (!(Test-Path $profile)) {
+  mkdir -force ([system.io.fileinfo]$profile).DirectoryName
+
+  echo '$env:PATH+=";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"' > $profile
+}
+
+#choco install --execution-timeout 0 -y VisualStudio2015Community --packageParameters "/AdminFile c:\packer\AdminDeployment-ws2012-vs2015.xml"
+
+# TODO: check $PATH for expected utilities
+# TODO: check include path for expected headers
+
