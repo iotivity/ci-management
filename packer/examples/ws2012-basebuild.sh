ba1f09adb84f1401190112750c9f278b3c48df91
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* renamed README.markdown to README.md
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.md
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* as OpenStack user-data scripts
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/examples/ws2012-basebuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script installs visual studio prereqs to a windows server 2012
+# packer image based on the cloudbase-init evaluation image
+
+set -e
+
+# https://www.packer.io/docs/builders/openstack.html
+
+mkdir -p temp log
+
+OS=ws2012
+TOOLCHAIN=basebuild
+TEMPLATE=${TOOLCHAIN}
+
+TMPDIR=./temp
+export PACKER_LOG=1
+TS=$(date +"%Y%m%dT%H%M%S")
+export PACKER_LOG_PATH="${PWD}/log/packer-${OS}-${TOOLCHAIN}-${TS}.log"
+
+${HOME}/bin/packer build   \
+  -var-file=vars/cloud-env.json   \
+  -var-file=vars/${OS}-${TOOLCHAIN}.json   \
+  templates/${TEMPLATE}-win.json
+
+unset -v TS
