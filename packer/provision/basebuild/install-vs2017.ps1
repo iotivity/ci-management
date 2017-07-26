6ace4326d4f8b46cc898d6c524ceeb155cfb5453
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* install windows-sdk-10.1 just before running the visual studio installer
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-vs2017.ps1
@@ -0,0 +1,51 @@
+echo "Installing Visual Studio 2017"
+
+choco install --execution-timeout 0 -y windows-sdk-10.1
+
+echo "Disk usage:"
+Get-WmiObject win32_logicaldisk
+
+$install_success_file = "c:\packer\vs-install-success"
+$was_installed=$FALSE
+
+Function Uninstall-VisualStudio() {
+  echo "to be implemented"
+#  Remove-Item $install_success_file
+}
+
+Function Install-VisualStudio() {
+  # Download and install VS2017
+  Write-Output "$(Get-Date) -- Download and install VS2017 --"
+  $vsSource="https://aka.ms/vs/15/release/vs_community.exe"
+  $vsDest="c:\packer\vs_community.exe"
+  c:\ProgramData\chocolatey\bin\curl.exe -k -o $vsDest $vsSource
+  Start-Process $vsDest -Wait -ArgumentList '--add Microsoft.VisualStudio.Component.Windows81SDK --add Microsoft.VisualStudio.Component.VC.140 --add Microsoft.VisualStudio.Component.Windows10SDK --add Microsoft.VisualStudio.Component.Windows10SDK.15063.Desktop --add Microsoft.VisualStudio.Component.Windows10SDK.15063.UWP --add Microsoft.Component.VC.Runtime.OSSupport --add Microsoft.VisualStudio.Component.VC.CoreIde --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.ComponentGroup.UWP.VC --add Microsoft.VisualStudio.Component.UWP.Support --add Microsoft.Component.NetFX.Native --add Microsoft.Net.Component.4.5.TargetingPack --add Microsoft.Component.MSBuild --add Microsoft.Net.Component.4.7.SDK --add Microsoft.Net.Component.4.7.TargetingPack --add Component.Android.SDK23 --quiet --wait --norestart'
+
+}
+
+if( Test-Path $install_success_file ){
+  Remove-Item $install_success_file
+  $was_installed=$TRUE
+
+  Uninstall-VisualStudio
+
+  Install-VisualStudio
+}else{
+  Install-VisualStudio
+}
+
+if( $lastExitCode -eq 0 ){
+  echo $null >> $install_success_file
+  echo "Visual Studio 2017 Installed"
+}else{
+#  Get-Content C:\Users\vagrant\AppData\Local\Temp\chocolatey\vs.log
+#  Get-WinEvent -FilterHashtable @{logname = 'setup'} | Format-Table timecreated, message -AutoSize -Wrap
+  echo "Visual Studio 2017 Installation failed"
+#  if ($was_installed -ne $TRUE) {
+#    exit 0
+#  }
+}
+
+# TODO: check $PATH for expected utilities
+# TODO: check include path for expected headers
+
