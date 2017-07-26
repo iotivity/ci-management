4b1f43c7ffc7a7eb8c8a05bc95910afa2be6a93c
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Packer definitions for VEXXHOST
    /* added support for ubuntu 16.04
     */
    
    Use chocolatey to install VS2015 development tools
    Install cloudbase-init to Windows Server 2012-R2 system
    Check out prereqs
    Prepare image to install Jenkins JNLP slave agent on first boot
    
    * Add README.md
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* as OpenStack user-data scripts
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/tizen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -ex
+set -o pipefail
+
+# Add Tizen tool repo, and install GBS
+RELEASE=$(lsb_release -r | awk '{print $2}')
+
+TIZEN_DEB_URL='http://download.tizen.org/tools/latest-release'
+
+if [ $RELEASE == '16.04' ]
+then
+
+  TIZEN_DEB_URL="$TIZEN_DEB_URL/Ubuntu_16.04/"
+
+elif [ $RELEASE == '12.04' ]
+then
+
+  TIZEN_DEB_URL="$TIZEN_DEB_URL/Ubuntu_12.04/"
+
+fi
+echo "deb $TIZEN_DEB_URL /" > /etc/apt/sources.list.d/tizen.list
+apt-get -y update
+apt-get -y install --force-yes gbs
