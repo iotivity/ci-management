d911ed8eb187d92b326b2e52c7dd9a66a5f4c971
C.J. Collier <cjcollier@linuxfoundation.org>
2017-07-26 14:55:19 -0700

    Add Windows Packer support for VEXXHOST
    /* separating vs2015 from other packages
     * replace my attempt at writing a vs2015 installer with chocolatey
     * add reboot between installation of chocolatey and packages
     * and between installation of packages and run of bootstrap.ps1
     */
    
    Use chocolatey to install VS2015 development tools to Windows Server
    2012-R2 system
    
    * Add README.markdown
    * ignore packer/var/cloud-env.json to avoid including sensitive
      information in gerrit
    * add provision/basebuild* and templates/basebuild* to provision
      common dependencies
    * add provision/basebuild/ for resources required for basebuild phase
    * add provision/baseline* and templates/baseline* to provision core
      minion environment
    * add provision/*_data* for configuring user settings
    * add vars/*.json for each platform on which we provision minions
    * add examples/*.sh for help in initial environment configuration
    
    Change-Id: I3a410011a15c90bf539338d75ce9ad03fe20af94
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ packer/provision/basebuild/install-pkgs.ps1
@@ -0,0 +1,13 @@
+choco install -y curl
+choco install -y dotnet4.5.1
+choco install -y cmake
+choco install -y git
+choco install -y sqlite
+choco install -y python2
+choco install -y pywin32
+choco install -y pip
+pip install scons
+choco install -y strawberryperl
+choco install -y KB2919442
+choco install -y kb2919355
+
