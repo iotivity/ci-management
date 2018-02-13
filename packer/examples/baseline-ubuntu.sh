4f146279fd2c7a713dc9f3a58f0ba82f786d1a08
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-13 22:33:42 +0000

    Correct stack to cloud
    common-packer uses "cloud_*" instead of "stack_*" ; correct for this
    
    Change-Id: I73dc74978e69f8ce675933ae9cca902e65c16b9f
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- packer/examples/baseline-ubuntu.sh
+++ packer/examples/baseline-ubuntu.sh
@@ -15,7 +15,7 @@ export PACKER_LOG_PATH="${PWD}/log/packer-ubuntu-baseline-${TS}.log"
 
 ${HOME}/bin/packer build   \
   -var-file=vars/cloud-env.json   \
-  -var-file=vars/ubuntu-12.04.json   \
+  -var-file=vars/ubuntu-12.04-baseline.json  \
   templates/baseline.json
 
 unset -v TS
