5c63cb0932aa407766c52396ad6dfc084a6f09c7
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-15 23:04:56 +0000

    Move creation of jenkins user to jenkins-scripts
    Creating Jenkins user through jenkins-scripts/create_jenkins_user.sh
    instead of during packer image build-out to match releng project
    procedures
    
    Change-Id: I788ce3bf60d382c87db7a6becedf996fa2e5299b
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jenkins-scripts/create_jenkins_user.sh
+++ jenkins-scripts/create_jenkins_user.sh
@@ -38,4 +38,4 @@ mkdir /w
 cp -r /home/${OS}/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
 # Generate ssh key for use by Robot jobs
 echo -e 'y\n' | ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa -t rsa
-chown -R jenkins:jenkins /home/jenkins/.ssh /w
+chown -R jenkins:jenkins /home/jenkins/.ssh /w /extlibs
--- packer/provision/cleanup.sh
+++ packer/provision/cleanup.sh
@@ -6,10 +6,3 @@ then
   swapoff /swap
   rm /swap
 fi
-
-# Ensure Jenkins user has the required file permissions
-chown -R jenkins-ci:jenkins-ci /home/jenkins-ci
-
-if [ -d /extlibs/ ]; then
-  chown -R jenkins-ci:jenkins-ci /extlibs/
-fi
