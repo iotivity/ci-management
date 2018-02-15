f3f8239ff4fb292bb685a8d95fa3160863cc03e7
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-15 20:39:21 +0000

    Make GBS-* directories writable
    Jenkins user does not have write access to GBS-* cache directories in
    their current state.  This change allows gbs to write to these
    directories during build time.
    
    Change-Id: Ic9168e123fe0013d06f0cd765fd19072c1ca51a1
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-tizen.sh
+++ jjb/iotivity/include-raw-iotivity-tizen.sh
@@ -2,6 +2,7 @@ BUILD_ROOTS="GBS-ROOT GBS-ROOT-RI GBS-ROOT-ES GBS-ROOT-OIC GBS-ROOT-RI-OIC"
 
 # link tizen's gbs cache to expected location
 IOTIVITYEXTLIB=/extlibs
+sudo chown -R ${USER} ${IOTIVITYEXTLIB}/GBS
 ln -s ${IOTIVITYEXTLIB}/GBS/* ${HOME}/
 
 for build_dir in $BUILD_ROOTS; do
