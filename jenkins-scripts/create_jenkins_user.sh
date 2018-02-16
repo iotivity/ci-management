e2a60b6506df623c208dd87672287bae12774ce6
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-16 23:29:10 +0000

    integrate old user creation script in to new
    use $ci_username instead of 'jenkins'
    hard-code in our public key
    chmod ssh dir and pub keys
    create swap
    
    Change-Id: Ic77e29254bdbeb85cbbae1cf16e21b0100643038
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jenkins-scripts/create_jenkins_user.sh
+++ jenkins-scripts/create_jenkins_user.sh
@@ -14,15 +14,17 @@
 #######################
 
 OS=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+ci_username="jenkins"
+ci_groupname="jenkins"
 
-useradd -m -s /bin/bash jenkins
+useradd -m -s /bin/bash ${ci_username}
 
 # Check if docker group exists
 grep -q docker /etc/group
 if [ "$?" == '0' ]
 then
   # Add jenkins user to docker group
-  usermod -a -G docker jenkins
+  usermod -a -G docker ${ci_username}
 fi
 
 # Check if mock group exists
@@ -30,12 +32,25 @@ grep -q mock /etc/group
 if [ "$?" == '0' ]
 then
   # Add jenkins user to mock group so they can build Int/Pack's RPMs
-  usermod -a -G mock jenkins
+  usermod -a -G mock ${ci_username}
 fi
 
-mkdir /home/jenkins/.ssh
+ci_pubkeys="/home/${ci_username}/.ssh/authorized_keys"
+
+mkdir /home/${ci_username}/.ssh/
 mkdir /w
-cp -r /home/${OS}/.ssh/authorized_keys /home/jenkins/.ssh/authorized_keys
+cp -r /home/${OS}/.ssh/authorized_keys ${ci_pubkeys}
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCld6N4H5SI5TxrKdZcXeI22Y+mPtiiFjP7y788dt8WN0KLaZZAJjeoUOq7qJGWqW3NUEXyzqeAxK0TIURjCHrnBA7vQm4wMZw22uvl/ZINx69ccxKya3NaY2KOMdSdczAJeZWj68o9n9Dzpj+kd1Qnt3xEIJGgdgYCgmnCrt5QAp8vnkzM+DVl38eCGzw4IwCo1ex6MwD0dwNzkDzeHD0YBneL7MY7klwB+7OSxgD/Wym0qNUNt446L8EZnMRJt9KjBEi1p6DGQGufUKQk1qqZC4e3u1AuS8O7HQP4khUNvNN14qpurtarUOEo9Ifveqkg2boZiBpklJ5Q70sgrN/Z ${ci_username}" >> "${ci_pubkeys}"
+
 # Generate ssh key for use by Robot jobs
 echo -e 'y\n' | ssh-keygen -N "" -f /home/jenkins/.ssh/id_rsa -t rsa
-chown -R jenkins:jenkins /home/jenkins/.ssh /w /extlibs
+
+chmod 700 /home/${ci_username}/.ssh
+chmod 600 ${ci_pubkeys}
+
+chown -R ${ci_username}:${ci_groupname} /home/${ci_username}/.ssh /w /extlibs
+
+dd if=/dev/zero of=/swap bs=1M count=16384
+mkswap /swap
+swapon /swap
