d27de3bd1c5c79bc0758798eeefcd5e69e468e30
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-16 22:44:38 +0000

    Sanitize packer var files
    * Ensure that each packer/vars/*.json file contains an ssh_user value
    * Normalize example script names
    * Remove cruft from packer/examples/
    
    Change-Id: If2fbf44403bddbff0000d1bd621b0f6a89e20411
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- packer/examples/baseline-centos-7.sh
+++ packer/examples/baseline-centos-7.sh
@@ -11,11 +11,11 @@ mkdir -p temp log
 TMPDIR=./temp
 export PACKER_LOG=1
 TS=$(date +"%Y%m%dT%H%M%S")
-export PACKER_LOG_PATH="${PWD}/log/packer-basebuild-ws2016-${TS}.log"
+export PACKER_LOG_PATH="${PWD}/log/packer-builder-${TS}.log"
 
 ${HOME}/bin/packer build   \
   -var-file=vars/cloud-env.json   \
-  -var-file=vars/basebuild-ws2016.json   \
-  templates/basebuild-win.json
+  -var-file=vars/centos-7.json   \
+  templates/baseline.json
 
 unset -v TS
