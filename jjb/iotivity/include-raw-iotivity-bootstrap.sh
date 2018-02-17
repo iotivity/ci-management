52345dd7a231214a189405e628cd432d12bff205
C.J. Collier <cjcollier@linuxfoundation.org>
2018-02-17 01:11:44 +0000

    Cache new extlibs and tidy code
    Fetch new extlibs libstrophe, rapidjson and wksxmpp_chat at packer
    init time.
    
    Reduce duplication in include-raw-iotivity-bootstrap.sh and include
    these new extlibs
    
    Change-Id: Ie6dcbc04f3dfbe72d502723d3ebf981d93921637
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-bootstrap.sh
+++ jjb/iotivity/include-raw-iotivity-bootstrap.sh
@@ -4,75 +4,39 @@ set -o pipefail
 #IOTIVITYEXTLIB=${IOTIVITYEXTLIB:-/extlibs}
 IOTIVITYEXTLIB=/extlibs
 
-rm -rf ${WORKSPACE}/extlibs/arduino/arduino-1.5.8
-rm -rf ${WORKSPACE}/extlibs/boost/boost_1_58_0
-rm -rf ${WORKSPACE}/extlibs/boost/boost_1_58_0.zip
-rm -rf ${WORKSPACE}/extlibs/cereal
-rm -rf ${WORKSPACE}/extlibs/tinycbor/tinycbor
-rm -rf ${WORKSPACE}/extlibs/gtest/gtest-1.7.0
-rm -rf ${WORKSPACE}/extlibs/gtest/google-release-1.7.0
-rm -rf ${WORKSPACE}/extlibs/hippomocks-master
-rm -rf ${WORKSPACE}/extlibs/android/ndk/android-ndk-r10d
-rm -rf ${WORKSPACE}/extlibs/android/sdk/android-sdk_r24.2
-rm -rf ${WORKSPACE}/extlibs/android/gradle/gradle-2.2.1
-rm -rf ${WORKSPACE}/extlibs/raxmpp/raxmpp
-rm -rf ${WORKSPACE}/extlibs/libcoap/libcoap
+for goner in arduino/arduino-1.5.8 \
+             boost/boost_1_58_0 \
+             boost/boost_1_58_0.zip \
+             cereal \
+             tinycbor/tinycbor \
+             gtest/gtest-1.7.0 \
+             gtest/google-release-1.7.0 \
+             hippomocks-master \
+             android/ndk/android-ndk-r10d \
+             android/sdk/android-sdk_r24.2 \
+             android/gradle/gradle-2.2.1 \
+             raxmpp/raxmpp \
+             libcoap/libcoap
+do
+  rm -rf ${WORKSPACE}/extlibs/${goner}
+done
 
-if [ ! -d "${WORKSPACE}/extlibs/arduino" ]
-then
-	mkdir ${WORKSPACE}/extlibs/arduino
-fi
+for extlib in arduino android android/ndk android/sdk android/gradle \
+              expat boost tinycbor raxmpp rapidjson gtest yaml \
+              mbedtls libcoap
+do
+  if [ ! -d "${WORKSPACE}/extlibs/${extlib}" ]
+  then
+        mkdir ${WORKSPACE}/extlibs/${extlib}
+  fi
+done
 
-if [ ! -d "${WORKSPACE}/extlibs/android" ]
-then
-	mkdir ${WORKSPACE}/extlibs/android
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/android/ndk" ]
-then
-	mkdir ${WORKSPACE}/extlibs/android/ndk
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/android/sdk" ]
-then
-	mkdir ${WORKSPACE}/extlibs/android/sdk
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/android/gradle" ]
-then
-	mkdir ${WORKSPACE}/extlibs/android/gradle
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/expat" ]
-then
-	mkdir ${WORKSPACE}/extlibs/expat
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/boost" ]
-then
-	mkdir ${WORKSPACE}/extlibs/boost
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/tinycbor" ]
-then
-	mkdir ${WORKSPACE}/extlibs/tinycbor
-fi
-
-if [ ! -d "${WORKSPACE}/extlibs/raxmpp" ]
-then
-	mkdir ${WORKSPACE}/extlibs/raxmpp
-fi
-
-
-ln -sv ${IOTIVITYEXTLIB}/arduino/arduino-1.5.8 ${WORKSPACE}/extlibs/arduino/arduino-1.5.8
 if [ -d "${IOTIVITYEXTLIB}/boost/boost_1_58_0" ]
 then
   cp -al "${IOTIVITYEXTLIB}/boost/boost_1_58_0" "${WORKSPACE}/extlibs/boost/"
 else
   unzip -oq ${IOTIVITYEXTLIB}/boost/boost_1_58_0.zip -d ${WORKSPACE}/extlibs/boost/
 fi
-ln -sv ${IOTIVITYEXTLIB}/boost/boost_1_58_0.zip ${WORKSPACE}/extlibs/boost/boost_1_58_0.zip
-ln -sv ${IOTIVITYEXTLIB}/cereal ${WORKSPACE}/extlibs/cereal
 
 TINYCBOR_VERSION='0.4.1'
 # always use the version that went through QA with each branch:
@@ -93,37 +57,25 @@ unzip -oq "${IOTIVITYEXTLIB}/tinycbor/v${TINYCBOR_VERSION}.zip" -d ${IOTIVITYEXT
 ln -sv "${IOTIVITYEXTLIB}/tinycbor/tinycbor-${TINYCBOR_VERSION}" ${WORKSPACE}/extlibs/tinycbor/tinycbor
 
 ln -sv ${IOTIVITYEXTLIB}/android/android-ndk-r10d.bin ${WORKSPACE}/extlibs/android/ndk/android-ndk-r10d
-ln -sv ${IOTIVITYEXTLIB}/android/sdk/android-sdk_r24.2 ${WORKSPACE}/extlibs/android/sdk/android-sdk_r24.2
-ln -sv ${IOTIVITYEXTLIB}/android/gradle/gradle-2.2.1 ${WORKSPACE}/extlibs/android/gradle/gradle-2.2.1
-ln -sv ${IOTIVITYEXTLIB}/expat/expat-2.1.0 ${WORKSPACE}/extlibs/expat/expat-2.1.0
-ln -sv ${IOTIVITYEXTLIB}/raxmpp/raxmpp ${WORKSPACE}/extlibs/raxmpp/raxmpp
-
-if [ ! -d "${WORKSPACE}/extlibs/gtest" ]
-then
-	mkdir ${WORKSPACE}/extlibs/gtest
-fi
-
-ln -sv ${IOTIVITYEXTLIB}/gtest/gtest-1.7.0 ${WORKSPACE}/extlibs/gtest/gtest-1.7.0
 ln -sv ${IOTIVITYEXTLIB}/gtest/gtest-1.7.0 ${WORKSPACE}/extlibs/gtest/google-release-1.7.0
-ln -sv ${IOTIVITYEXTLIB}/hippomocks-master ${WORKSPACE}/extlibs/hippomocks-master
 
-if [ ! -d "${WORKSPACE}/extlibs/yaml" ]
-then
-	mkdir ${WORKSPACE}/extlibs/yaml
-fi
-ln -sv ${IOTIVITYEXTLIB}/yaml/yaml ${WORKSPACE}/extlibs/yaml/yaml
-
-ln -sv ${IOTIVITYEXTLIB}/sqlite3/sqlite3.c ${WORKSPACE}/extlibs/sqlite3/sqlite3.c
-ln -sv ${IOTIVITYEXTLIB}/sqlite3/sqlite3.h ${WORKSPACE}/extlibs/sqlite3/sqlite3.h
-
-if [ ! -d "${WORKSPACE}/extlibs/mbedtls" ]
-then
-	mkdir ${WORKSPACE}/extlibs/mbedtls
-fi
-cp -al ${IOTIVITYEXTLIB}/mbedtls/mbedtls ${WORKSPACE}/extlibs/mbedtls/mbedtls
-
-if [ ! -d "${WORKSPACE}/extlibs/libcoap" ]
-then
-	mkdir ${WORKSPACE}/extlibs/libcoap
-fi
-cp -al ${IOTIVITYEXTLIB}/libcoap/libcoap ${WORKSPACE}/extlibs/libcoap/libcoap
+for extlib in android/sdk/android-sdk_r24.2 \
+              android/gradle/gradle-2.2.1 \
+              expat/expat-2.1.0 \
+              raxmpp/raxmpp \
+              libstrophe/libstrophe \
+              rapidjson/rapidjson \
+              wksxmppxep/wksxmpp_chat \
+              gtest/gtest-1.7.0 \
+              hippomocks-master \
+              arduino/arduino-1.5.8 \
+              boost/boost_1_58_0.zip \
+              cereal \
+              yaml/yaml \
+              sqlite3/sqlite3.c \
+              sqlite3/sqlite3.h \
+              mbedtls/mbedtls \
+              libcoap/libcoap
+do
+  ln -sv ${IOTIVITYEXTLIB}/${extlib} ${WORKSPACE}/extlibs/${extlib}
+done
