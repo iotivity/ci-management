10f59d1136baab785a28b2af41459629cd9247f6
C.J. Collier <cjcollier@linuxfoundation.org>
2018-03-05 23:37:17 +0000

    the -al copy fails sometimes
    replacing with -R
    
    Change-Id: I50f8f19e5117cca742f50a6cc2120256b9d7369a
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/iotivity/include-raw-iotivity-bootstrap.sh
+++ jjb/iotivity/include-raw-iotivity-bootstrap.sh
@@ -120,10 +120,10 @@ if [ ! -d "${WORKSPACE}/extlibs/mbedtls" ]
 then
 	mkdir ${WORKSPACE}/extlibs/mbedtls
 fi
-cp -al ${IOTIVITYEXTLIB}/mbedtls/mbedtls ${WORKSPACE}/extlibs/mbedtls/mbedtls
+cp -R ${IOTIVITYEXTLIB}/mbedtls/mbedtls ${WORKSPACE}/extlibs/mbedtls/mbedtls
 
 if [ ! -d "${WORKSPACE}/extlibs/libcoap" ]
 then
 	mkdir ${WORKSPACE}/extlibs/libcoap
 fi
-cp -al ${IOTIVITYEXTLIB}/libcoap/libcoap ${WORKSPACE}/extlibs/libcoap/libcoap
+cp -R ${IOTIVITYEXTLIB}/libcoap/libcoap ${WORKSPACE}/extlibs/libcoap/libcoap
