56f0c80ac23a312ecac4bb5d02e3801ac0e21641
C.J. Collier <cjcollier@linuxfoundation.org>
2018-03-28 13:54:21 +0000

    combine windows preparation scripts
    Change-Id: Ifdf9b59668ef2c45ec43049b0454622d94e95d01
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- packer/provision/basebuild/win-prep.bat
+++ packer/provision/basebuild/win-prep.bat
@@ -1,3 +1,6 @@
+net accounts /maxpwage:unlimited::
+enable RDP:http://technet.microsoft.com/en-us/library/cc782195(v=ws.10).aspx
+%SystemRoot%\System32\reg.exe ADD "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 <!-- :
 @setlocal EnableDelayedExpansion EnableExtensions
 @for %%i in (%~dp0\_packer_config*.cmd) do @call "%%~i"
@@ -31,3 +34,28 @@ exit /b
   AutoUpdateSettings.Save
   AutoUpdateSettings.Refresh
 </script></job>
+
+@setlocal EnableDelayedExpansion EnableExtensions
+@for %%i in (%~dp0\_packer_config*.cmd) do @call "%%~i"
+@if defined PACKER_DEBUG (@echo on) else (@echo off)
+
+title Disabling UAC. Please wait...
+
+echo ==^> Disabling UAC
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f /v EnableLUA /t REG_DWORD /d 0
+echo Reboot required to make this change effective.
+
+:exit0
+
+ver>nul
+goto :exit
+
+:exit1
+verify other 2>nul
+
+%SystemRoot%\System32\reg.exe ADD HKLM\SYSTEM\CurrentControlSet\Control\Power\ /v HibernateFileSizePercent /t REG_DWORD /d 0 /f
+%SystemRoot%\System32\reg.exe ADD HKLM\SYSTEM\CurrentControlSet\Control\Power\ /v HibernateEnabled /t REG_DWORD /d 0 /f
+netsh advfirewall firewall add rule name="All ICMP V4" dir=in action=allow protocol=icmpv4
+netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
+
+:exit
